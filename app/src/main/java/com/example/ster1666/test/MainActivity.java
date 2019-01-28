88774d9e342c3f31c502efa70eb0a029316a1f46
ster1666 <erikstrand00@student.ju.se>
2019-01-28 14:46:43 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ster1666/test/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ster1666.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
